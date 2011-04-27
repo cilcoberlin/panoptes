44eddd0aee2480bcd78840f14683fc0864bcb6b0
Justin <justin.locsei@oberlin.edu>
2011-04-27 15:56:34 -0400

    Added a skeleton for login and logout pages.

--- panoptes/urls.py
+++ panoptes/urls.py
@@ -1,5 +1,6 @@
 
 from django.conf.urls.defaults import *
+from django.views.generic.simple import redirect_to
 
 api_v1_patterns = patterns('',
 	(r'^tracking/', include('panoptes.tracking.api.urls'))
@@ -13,8 +14,18 @@ api_patterns = patterns('',
 	(r'^versions/', include(api_version_patterns))
 )
 
+account_patterns = patterns('django.contrib.auth.views',
+	url(r'login/$', 'login', {'template_name': 'panoptes/accounts/login.html'}, 'login'),
+	url(r'logout/$', 'logout', {'template_name': 'panoptes/accounts/logout.html'}, 'logout')
+)
+
 urlpatterns = patterns('',
 
+	(r'^accounts/', include(account_patterns)),
 	(r'^analysis/', include('panoptes.analysis.urls')),
-	(r'^api/', include(api_patterns))
+	(r'^api/', include(api_patterns)),
+	
+	#  Redirect an index view to the analysis index
+	url(r'^/?$', redirect_to, {'url': 'analysis/', 'permanent': False}),
+	
 )
