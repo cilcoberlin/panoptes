565c90f15bf34b8d27b935f3f2f1edec280ebf68
Justin <justin.locsei@oberlin.edu>
2011-04-29 10:07:54 -0400

    Fixed broken weekday name function.

--- panoptes/core/utils/constants.py
+++ panoptes/core/utils/constants.py
@@ -5,11 +5,11 @@ from django.utils.translation import ugettext_lazy as _
 def weekday_name(iso_number):
 	"""Return the weekday name for the given ISO weekday number or a blank string."""
 	try:
-		return _WEEKDAYS[iso_number + 1]
-	except KeyError:
+		return _WEEKDAYS[iso_number - 1]
+	except IndexError:
 		return u""
 
-WEEKDAYS = tuple([_WEEKDAYS[i] for i in sorted(_WEEKDAYS.keys())]) 
+WEEKDAYS = tuple([_WEEKDAYS[i] for i in sorted(_WEEKDAYS.keys())])
 WEEKDAYS_ABBR = tuple([_WEEKDAYS_ABBR[i] for i in sorted(_WEEKDAYS_ABBR.keys())])
 WEEKDAYS_SINGLE_LETTER = (_('M'), _('T'), _('W'), _('R'), _('F'), _('S'), _('U'))
 
