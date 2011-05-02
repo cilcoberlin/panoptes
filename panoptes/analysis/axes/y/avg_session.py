a3fb38370c3e13e09d1f715f9c468bef9678194f
Justin <justin.locsei@oberlin.edu>
2011-05-02 10:15:20 -0400

    Improved the performance of average session length queries.

--- panoptes/analysis/axes/y/avg_session.py
+++ panoptes/analysis/axes/y/avg_session.py
@@ -3,34 +3,56 @@ from django.utils.translation import ugettext_lazy as _
 
 from panoptes.analysis.axes.y import YAxis
 
+import copy
+
 class Axis(YAxis):
 	"""A y-axis of the average session length."""
 
 	name = _("average session length")
 	slug = "avg-session-length"
 
-	def _average_length_for_queryset(self, sessions):
-		"""Return the average session length for the given Session queryset."""
-		try:
-			return sum([(session.end - session.start).seconds for session in sessions if session.end]) / sessions.count()
-		except ZeroDivisionError:
-			return 0
+	def _average_length_for_queryset(self, sessions, field_name, x_values):
+		"""
+		Return the average length for the given sessions, grouped by field.
+
+		Arguments:
+		sessions -- a Session queryset
+		field_name -- the string name of the field to group by
+		x_values -- a list of x-values for which to generate data
+
+		Returns: a list of integers as long as x_values
+
+		"""
+
+		by_field = dict(zip(x_values, [0] * len(x_values)))
+		counts = copy.copy(by_field)
+		for session in sessions.iterator():
+			field_key = getattr(session, field_name)
+			by_field[field_key] += (session.end - session.start).seconds
+			counts[field_key] += 1
+
+		y_values = []
+		for x_value in x_values:
+			try:
+				y_value = by_field[x_value] / counts[x_value]
+			except (ZeroDivisionError, KeyError):
+				y_value = 0
+			y_values.append(y_value)
+		return y_values
 
 	def day_values(self, x_values, sessions, filters):
 		"""
 		Return a list of the average session length of each date instance in the
 		`x_values` list.
 		"""
-		return [self._average_length_for_queryset(sessions.filter(start_date=use_date))
-			for use_date in x_values]
+		return self._average_length_for_queryset(sessions, 'start_date', x_values)
 
 	def workstation_values(self, x_values, sessions, filters):
 		"""
 		Return a list of the average session length for each Workstation
 		instance in the `x_values` list.
 		"""
-		return [self._average_length_for_queryset(sessions.filter(workstation=workstation))
-			for workstation in x_values]
+		return self._average_length_for_queryset(sessions, 'workstation', x_values)
 
 	def render_value(self, value):
 		"""Render the average session length in seconds as an HH:MM string."""
