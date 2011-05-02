1288210ad1f592d1f3a21e7df21aca98219cacfe
Justin <justin.locsei@oberlin.edu>
2011-05-02 15:11:25 -0400

    Rewrote the Windows client installer.

--- client/install_windows.cmd
+++ client/install_windows.cmd
@@ -1,28 +1,27 @@
+@echo off
 
-Const INSTALL_PATH = "C:\Program Files\Panoptes\"
-Const TRACKER_NAME = "panoptes.py"
+set INSTALL_PATH_QUOTELESS=C:\Program Files\Panoptes\
+set INSTALL_PATH="%INSTALL_PATH_QUOTELESS%"
+set TRACKER_NAME=panoptes.py
 
-' Get the Panoptes URL from the user
-Dim panoptesURL 
-panoptesURL = InputBox("Please enter the full URL to your Panoptes installation", "Panoptes Installer")
+rem Get the Panoptes URL from the user or a command-line argument
+if "%1"=="" goto from_user
+set panoptes_url=%1
+goto end_url
+:from_user
+set /P panoptes_url="Please enter the full URL to your Panoptes installation: "
+:end_url
 
-' Create Panoptes' file structure
-Dim oFile 
-Set oFile = CreateObject("Scripting.FileSystemObject")
-If oFile.FolderExists(INSTALL_PATH) <> True Then
-    oFile.CreateFolder INSTALL_PATH
-End If
-oFile.CopyFile "tracker\" & TRACKER_NAME, INSTALL_PATH, True
-Set oFile = Nothing
+rem Create the Panoptes file structure
+if exist %INSTALL_PATH% rmdir /S /Q %INSTALL_PATH%
+mkdir %INSTALL_PATH%
+copy tracker\%TRACKER_NAME% %INSTALL_PATH%
+echo All files have been copied!
 
-' Install the tracker service with the user-provided URL
-Dim oShell, controlSvc
-Set oShell = CreateObject("WScript.Shell")
-controlSvc = "python """ & INSTALL_PATH & TRACKER_NAME & """ "
-oShell.Run controlSvc & "--wait 60 stop", 0, True
-oShell.Run controlSvc & "remove", 0, True
-oShell.Run controlSvc & "--startup auto install --url " & panoptesURL, 0, True
-oShell.Run controlSvc & "--wait 60 start", 0, True
-Set oShell = Nothing
-
-MsgBox("The Panoptes tracker was successfully installed")
+rem Install the tracker service with the user-provided URL
+set tracker=python "%INSTALL_PATH_QUOTELESS%%TRACKER_NAME%"
+%tracker% --wait 60 stop
+%tracker% remove
+%tracker% --startup auto install --url %panoptes_url%
+%tracker% --wait 60 start
+echo The Panoptes tracker was successfully configured and loaded!
