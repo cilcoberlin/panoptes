5550f1efcbd8e93d1745505223b260a394a8c423
Justin <justin.locsei@oberlin.edu>
2011-05-03 11:02:33 -0400

    Fixed a crash that occurs when running Panoptes via WSGI.

--- panoptes/admin.py
+++ panoptes/admin.py
@@ -1,4 +1,10 @@
 
+#  This import exists to allow many of the analysis classes, which use a
+#  metaclass registry, to register themselves before the admin autodiscover
+#  starts looking for admin modules.  If it is not here, things like the lens
+#  choice field crash, since the lenses have yet to be initialized.
+import panoptes.models
+
 import panoptes.accounts.admin
 import panoptes.analysis.admin
 import panoptes.core.admin
