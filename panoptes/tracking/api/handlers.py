e0b4bbca2068e444d95f255c62a3808ba0aa17f5
Justin <justin.locsei@oberlin.edu>
2011-10-03 15:49:20 -0400

    A location's activity is now available via the REST API.
    A new API URL now provides information on the total number of sessions that have occurred at a location between optional datetime boundaries.

--- panoptes/tracking/api/handlers.py
+++ panoptes/tracking/api/handlers.py
@@ -4,34 +4,96 @@ from panoptes.core.utils.api import validate
 from panoptes.tracking.forms import CreateSessionForm, EndSessionForm
 from panoptes.tracking.models import AccountFilter
 
+from dateutil.parser import parser as dateutil_parser
 from piston.handler import BaseHandler
 from piston.utils import rc
 
-class LocationHandler(BaseHandler):
+class LocationInfoHandler(BaseHandler):
 	"""Handler for locations."""
 
 	allowed_methods = ('GET',)
 
-	def read(self, request, slug=None):
-		"""Return information about a location, with the following keys:
+	def read(self, request, location_slug=None):
+		"""Return basic information about a location.
 
-		name               - The full display name of the location
+		This information is available in the JSON response via the following keys:
 
-		open_workstations  - The number of workstations currently available
+		    name               - The full display name of the location
 
-		total_workstations - The total number of workstations at the location
+			open_workstations  - The number of workstations currently available
+
+			total_workstations - The total number of workstations at the location
+
+		If the location slug provided does not map to a valid location, an HTTP
+		400 bad request response is returned.
+
+		:param location_slug: a slug identifying a location
 
 		"""
-
 		try:
-			location = Location.objects.get(slug=slug)
+			location = Location.objects.get(slug=location_slug)
 		except Location.DoesNotExist:
 			return rc.BAD_REQUEST
-
 		return {
 			'name': location.name,
 			'open_workstations': location.open_workstation_count,
 			'total_workstations': location.total_workstation_count
+		}
+
+class LocationActivityHandler(BaseHandler):
+	"""Handler for location user information."""
+
+	allowed_methods = ('GET',)
+
+	def read(self, request, location_slug=None, start_dt=None, end_dt=None):
+		"""Return information about the activity at a location.
+
+		This activity is available to the caller by accessing the following keys
+		of the generated JSON response:
+
+		    total_users  - The total number of users that logged a session
+
+		This method can be passed two optional parameters to specify datetimes
+		after or before which sessions were logged, being the :param:start_dt
+		and :param:end_dt arguments.  Each of these must be a datetime string
+		formatted according to RFC 3339 (i.e., 1937-01-01T12:00:27.87+00:20).
+		These parameters can be used to specify a date window during which all
+		counted sessions must have occurred.
+
+		If the location slug provided does not map to a valid location, or if
+		either of the optional datetimes given is not properly formatted, an HTTP
+		400 bad request response is returned.
+
+		:param location_slug: a slug identifying a location
+		:key start_dt: an RFC 3339 datetime after which any counted session must be logged
+		:key end_dt: an RFC 3339 datetime before which any counted session must be logged
+
+		"""
+
+		# Build the required location and optional datetime filters
+		try:
+			location = Location.objects.get(slug=location_slug)
+		except Location.DoesNotExist:
+			return rc.BAD_REQUEST
+		else:
+			filter_args = {'location': location}
+
+		parser = dateutil_parser()
+		if start_dt:
+			try:
+				filter_args['start_date'] = parser.parse(start_dt)
+			except ValueError:
+				return rc.BAD_REQUEST
+		if end_dt:
+			try:
+				filter_args['end_date'] = parser.parse(end_dt)
+			except ValueError:
+				return rc.BAD_REQUEST
+
+		# Return counts for the given sessions
+		sessions = Session.objects.filter_sessions(**filter_args)
+		return {
+			'total_users': sessions.count(),
 		}
 
 class SessionHandler(BaseHandler):
@@ -57,9 +119,7 @@ class SessionHandler(BaseHandler):
 		             reporting usage, or an arbitrary value. This value is used
 		             to control whether or not the session is tracked.
 		"""
-
 		data = request.form.cleaned_data
-
 		if AccountFilter.objects.is_user_loggable(data['user'], data['workstation']):
 			session = Session.objects.start_session(data['workstation'], data['os'])
 			return rc.CREATED if session else rc.BAD_REQUEST
@@ -72,11 +132,11 @@ class SessionHandler(BaseHandler):
 		End an existing session using the given raw POST data, whose keys and
 		their function are explained below.
 
-		mac -    The MAC address of the machine. For the session to be
+		mac    - The MAC address of the machine. For the session to be
 		         logged, a Workstation instance must exist with this MAC address
 		         and have a True `track` attribute.
 
-		apps -   A comma-separated string of applications used, with optional
+		apps   - A comma-separated string of applications used, with optional
 		         start and end dates of usage reported as ISO 8601 timestamps,
 		         all separated with a "#" character. If no time data is
 		         available, a 0 should be provided instead. Each grouping of
@@ -88,8 +148,6 @@ class SessionHandler(BaseHandler):
 		offset - A positive or negative integer representing the number of
 		         seconds that should be added to the session's reported end time.
 		"""
-
 		data = request.form.cleaned_data
-
 		session = Session.objects.end_session(data['workstation'], data['apps'], data['offset'])
 		return rc.ALL_OK if session else rc.BAD_REQUEST
--- panoptes/tracking/api/urls.py
+++ panoptes/tracking/api/urls.py
@@ -1,10 +1,16 @@
 
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import include, patterns, url
 
 from panoptes.core.utils.api import CSRFExemptResource
-from panoptes.tracking.api.handlers import LocationHandler, SessionHandler
+from panoptes.tracking.api.handlers import LocationActivityHandler, LocationInfoHandler, SessionHandler
+
+location_patterns = patterns('',
+	url(r'^info/$', CSRFExemptResource(handler=LocationInfoHandler)),
+	url(r'^activity/$', CSRFExemptResource(handler=LocationActivityHandler)),
+	url(r'^activity/from/(?P<start_dt>[^\/]+)/to/(?P<end_dt>[^\/]+)/$', CSRFExemptResource(handler=LocationActivityHandler)),
+)
 
 urlpatterns = patterns('',
-	url(r'^locations/(?P<slug>[^\/]+)/$', CSRFExemptResource(handler=LocationHandler)),
+	(r'^location/(?P<location_slug>[^\/]+)/', include(location_patterns)),
 	url(r'^sessions/$', CSRFExemptResource(handler=SessionHandler))
 )
